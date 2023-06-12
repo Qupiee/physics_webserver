2cfabf25665fa7f627ab1cb4901667644a6807b3
irumaa7 <petrocenko900@gmail.com>
2023-06-12 08:29:19 +0700

    server

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using physics_webserver.Models;
+using System.Data.SqlClient;
+
+namespace physics_webserver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Registration")]
+        public string register(User user)
+        {
+            string query = "insert into _User('UserRole', 'UserLogin', 'UserPassword') values('"+user.UserRole +"','"+ user.UserLogin +"','"+ user.UserPassword +"')";
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("PhysicsConnection").ToString());
+            SqlCommand command = new SqlCommand(query, connection);
+            int i = command.ExecuteNonQuery();
+            if (i > 0)
+            {
+                return "Пользователь зарегистрирован";
+            }
+            else
+            {
+                return "Ошибка ввода данных";
+            }
+        }
+    }
+}
